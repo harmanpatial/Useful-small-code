a145301c25903a4dab21a5489cd65d99f4179513
Harman Patial <harmanpatial@Harman-Macbook.local>
2012-09-15 20:28:44 -0700

    Added the javascript to add color to a div.
    Pick a free color and color a div with it.

---
+++ colorADiv.js
@@ -0,0 +1,57 @@
+// PICKING COLOR module.
+
+// Pick a color.
+function pickColor()
+{
+        for (var i=0; i <this.pickedColor.length; i++) {
+                if (this.pickedColor[i] == 0) {
+                        this.pickedColor[i] = 1;
+                        return this.colors[i];
+                }
+        }
+        return null;
+}
+
+// Free a color.
+function freeColor(color)
+{
+        for (var i=0; i <this.colors.length; i++) {
+                if ((this.colors[i]).toLowerCase() == color.toLowerCase()) {
+                        this.pickedColor[i] = 0;
+                        console.log("Freed the color : " + color);
+                        return;
+                }
+        }
+}
+
+// Change the rgb to hex code.
+// rgb is how we get the color from the div.
+function hexc(colorval) {
+    var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    if (parts == null) {
+        return null;
+    }
+    delete(parts[0]);
+    for (var i = 1; i <= 3; ++i) {
+        parts[i] = parseInt(parts[i]).toString(16);
+        if (parts[i].length == 1) parts[i] = '0' + parts[i];
+    }
+    return(color = '#' + parts.join(''));
+}
+
+
+$$ How to pick the color
+//var selColor = pickColor();                                     
+//console.log("Picked color = " + selColor);                      
+//tempObj.css('background', selColor);                            
+//tempObj.css('color', '#ffff00');                                
+//tempObj.css('background', '-webkit-linear-gradient(top, #000fff 0%,#000fff 50%,#ffff00 50%,#ffff00 100%);');                                               
+
+$$ How to free a color
+//var bgColor = $(this).css('backgroundColor');
+//console.log("bgColor = " + bgColor);
+//if (bgColor != null) {
+//	console.log("About to free the color" + hexc(bgColor));
+//	freeColor(hexc(bgColor));
+//}
+
